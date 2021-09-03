dadf919fc66b99ae05642f95f6554e05e39ccf88
Pedro Felipe <pftavares357@gmail.com>
2021-09-03 14:43:13 -0300

    Primeiro Commit

---
+++ android/app/src/main/kotlin/com/example/tela_simples/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.tela_simples
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'page_room.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: RoomPage(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
